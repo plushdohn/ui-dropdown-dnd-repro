dce0cb23a8f653933aafb84277845a5829108be7
plushdohn <lucafarci@protonmail.com>
2024-05-30 20:56:07 +0200

    repro

--- playground/app.vue
+++ playground/app.vue
@@ -1,20 +1,16 @@
+<script setup lang="ts">
+import { VueDraggable } from 'vue-draggable-plus'
+
+const items = [[{
+  label: 'test',
+}]]
+</script>
+
 <template>
   <UContainer class="min-h-screen flex items-center">
-    <UCard class="flex-1" :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50', ring: 'ring-1 ring-gray-300 dark:ring-gray-700', divide: 'divide-y divide-gray-300 dark:divide-gray-700', header: { base: 'font-bold' } }">
-      <template #header>
-        Welcome to the playground!
-      </template>
-
-      <p class="text-gray-500 dark:text-gray-400">
-        Try your components here!
-      </p>
-    </UCard>
+    <UDropdown :items="items" />
   </UContainer>
 </template>
-
-<script setup>
-
-</script>
 
 <style>
 body {
